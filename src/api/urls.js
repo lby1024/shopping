b66d2edfbdbc49cb1c3317f1f5ebfda40b210be2
lby1024 <1028524431@qq.com>
2018-09-10 15:06:04 +0800

    1

--- src/api/urls.js
+++
@@ -1,8 +0,0 @@
-
-
-export default {
-    base: 'http://smile.hucheng123.xin',
-    detail: '/api/getGoodsDetail',            // 获取详情页data
-    category: '/api/getCategory',             // 获取分类数据
-    goods: '/api/getMallGoods',               // 获取商品list
-}
